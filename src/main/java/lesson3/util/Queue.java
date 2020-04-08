0f30845e50404f83e0d34c535b8eaf331a9333af
maxkru <max_kru@hotmail.com>
2020-04-08 19:27:46 +0300

    Queue added

---
+++ src/main/java/lesson3/util/Queue.java
@@ -0,0 +1,54 @@
+package lesson3.util;
+
+import java.util.NoSuchElementException;
+
+public class Queue<T> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private T[] arr;
+    private int back, front;
+    private int size;
+
+    public Queue(int capacity) {
+        arr = (T[]) new Object[capacity];
+        back = front = size = 0;
+    }
+
+    public Queue() {
+        arr = (T[]) new Object[DEFAULT_CAPACITY];
+        back = front = size = 0;
+    }
+
+    public void add(T t) {
+        if (size == arr.length)
+            throw new IllegalStateException("Queue overflow");
+        arr[front] = t;
+        front = (front + 1) % arr.length;
+        size++;
+    }
+
+    public T remove() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        T result = arr[back];
+        arr[back] = null;
+        back = nextIndex(back);
+        size--;
+        return result;
+    }
+
+    public T element() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        return arr[back];
+    }
+
+    private int nextIndex(int i) {
+        return (i + 1) % arr.length;
+    }
+
+    public int size() {
+        return size;
+    }
+}
--- src/main/java/lesson3/util/Stack.java
+++ src/main/java/lesson3/util/Stack.java
@@ -4,45 +4,45 @@ import java.util.EmptyStackException;
 
 public class Stack<T> {
     private T[] arr;
-    private int pointer;
+    private int top;
     private static final int DEFAULT_CAPACITY = 10;
 
     public Stack(int capacity) {
         arr = (T[]) new Object[capacity];
-        pointer = -1;
+        top = -1;
     }
 
     public Stack() {
         arr = (T[]) new Object[DEFAULT_CAPACITY];
-        pointer = -1;
+        top = -1;
     }
 
     public void push(T t) {
         if (isFull()) {
             throw new StackOverflowError();
         }
-        arr[++pointer] = t;
+        arr[++top] = t;
     }
 
     public boolean isFull() {
-        return pointer + 1 == arr.length;
+        return top + 1 == arr.length;
     }
 
     public T pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
-        return arr[pointer--];
+        return arr[top--];
     }
 
     public T peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
-        return arr[pointer];
+        return arr[top];
     }
 
     public boolean isEmpty() {
-        return pointer == -1;
+        return top == -1;
     }
 }
