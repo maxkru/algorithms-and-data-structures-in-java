af96016f060ff1b18d8f8817105a1a8db36f6414
maxkru <max_kru@hotmail.com>
2020-04-30 10:34:55 +0300

    Excel parsing

--- src/main/java/lesson7/company/Subsidiary.java
+++ src/main/java/lesson7/company/Subsidiary.java
@@ -6,10 +6,18 @@ public class Subsidiary {
     final double actual;
     final double forecast;
 
-    public Subsidiary(int id, int parentId, double actual, double forecast) {
+    // в задании этого поля нет, но без него не обойтись
+    final String name;
+
+    public Subsidiary(int id, int parentId, String name, double actual, double forecast) {
         this.id = id;
         this.parentId = parentId;
+        this.name = name;
         this.actual = actual;
         this.forecast = forecast;
+    }
+
+    public double planCompletionPercent() {
+        return actual / forecast * 100.;
     }
 }
--- src/main/java/lesson7/excel/Main.java
+++ src/main/java/lesson7/excel/Main.java
@@ -1,17 +1,58 @@
 package lesson7.excel;
 
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import lesson7.company.HeadPool;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class Main {
 
-    private final static String filepath = "input.xls";
+    private final static String filepath = "CompanyIndicators.xlsx";
+    private final static int SHEET_NUMBER = 0;
+
+    private final static int HEAD_COLUMN_NUMBER = 0;
+    private final static int SUBSIDIARY_COLUMN_NUMBER = 1;
+    private final static int FORECAST_COLUMN_NUMBER = 2;
+    private final static int ACTUAL_COLUMN_NUMBER = 3;
+
 
     public static void main(String[] args) throws IOException {
         Workbook workbook = WorkbookFactory.create(new File(filepath));
+        Sheet sheet = workbook.getSheetAt(SHEET_NUMBER);
+
+        HeadPool heads = new HeadPool();
+
+
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        // первая строка - имена столбцов
+        rowIterator.next();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            // столбцы листа: Head name | Subsidiary name | forecast | actual
+
+            Cell headCell = row.getCell(HEAD_COLUMN_NUMBER);
+            String headName = headCell.getStringCellValue();
+            // Head name - допустимы смёрженные ячейки
+            if (headName.isEmpty()) {
+                for (CellRangeAddress cellRangeAddress : sheet.getMergedRegions()) {
+                    if (cellRangeAddress.isInRange(headCell)) {
+                        headName = sheet.getRow(cellRangeAddress.getFirstRow()).getCell(HEAD_COLUMN_NUMBER).getStringCellValue();
+                    }
+                }
+            }
+            String subsidiaryName = row.getCell(SUBSIDIARY_COLUMN_NUMBER).getStringCellValue();
+            double forecast = row.getCell(FORECAST_COLUMN_NUMBER).getNumericCellValue();
+            double actual = row.getCell(ACTUAL_COLUMN_NUMBER).getNumericCellValue();
+            heads.addSubsidiaryToHead(headName, subsidiaryName, forecast, actual);
+        }
+
+        heads.printPlanCompletionPercent();
     }
 
 
